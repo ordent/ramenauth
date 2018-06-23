49281ae8916e5348954e7f5a77595cb2f639bc64
Dimas Satrio <dimassrio@gmail.com>
2018-06-24 04:33:37 +0700

    fixing login with email username phone

--- src/Manager/AuthManager.php
+++ src/Manager/AuthManager.php
@@ -13,7 +13,7 @@ class AuthManager{
         if($validator->fails()){
             throw ValidationException::withMessages($validator->errors()->getMessages());
         }
-        $type = head(array_intersect(array_keys(array_except($request->all(), 'password')), ['email', 'password', 'username']));
+        $type = head(array_intersect(array_keys(array_except($request->all(), 'password')), ['email', 'phone', 'username']));
         $roles = $request->input('roles', false);
         $password = $request->input('password');
         try{
